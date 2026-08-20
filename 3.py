3601a5cec38f12b55c912b968ed679fcb19621ad
prathik0007 <prathikganiga202@gmail.com>
2026-08-20 22:41:21 +0530

    Move all zeros to the end

--- 3.py
+++ 3.py
@@ -1,27 +1,13 @@
-T = int(input())
+arr = [0, 1, 0, 3, 12]
 
-for _ in range(T):
-    n, target = map(int, input().split())
-    arr = list(map(int, input().split()))
+result = []
 
-    freq = {}
-    pairs = []
+for num in arr:
+    if num != 0:
+        result.append(num)
 
-    for num in arr:
-        complement = target - num
+for num in arr:
+    if num == 0:
+        result.append(num)
 
-        if complement in freq:
-            for _ in range(freq[complement]):
-                a = min(num, complement)
-                b = max(num, complement)
-                pairs.append([a, b])
-
-        freq[num] = freq.get(num, 0) + 1
-
-    pairs.sort()
-
-    if len(pairs) == 0:
-        print("-1 -1")
-    else:
-        for pair in pairs:
-            print(pair[0], pair[1])
+print(result)
